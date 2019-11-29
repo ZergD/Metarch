2b74d886783cea51e8cf302318648a01007bb307
MM <marc.mozgawa@gmail.com>
2019-11-30 00:28:34 +0100

    added a RectButton class.

---
+++ metarch/qt_gui/scene_objects/rect_buttons.py
@@ -0,0 +1,17 @@
+from PySide2.QtWidgets import QGraphicsItem
+
+
+class RectButton(QGraphicsItem):
+    def __init__(self):
+        super(RectButton, self).__init__()
+        pass
+
+
+
+
+
+
+
+
+
+
