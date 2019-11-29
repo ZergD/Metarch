b787d4ad2bccdeffb852358a1ea5ff2e063d2d9b
MM <marc.mozgawa@gmail.com>
2019-11-29 23:25:20 +0100

    added Key Q Press Event to quit.

--- metarch/qt_gui/antares_launcher_view.py
+++ metarch/qt_gui/antares_launcher_view.py
@@ -1,13 +1,15 @@
-from PySide2.QtWidgets import QGraphicsView
+from PySide2.QtWidgets import QGraphicsView, QApplication
+from PySide2.QtCore import Qt
 
 
 class AntaresLauncherView(QGraphicsView):
     def __init__(self, scene):
         super(AntaresLauncherView, self).__init__(scene=scene)
 
-
-
-
+    def keyPressEvent(self, qkey_event):
+        if qkey_event.key() == Qt.Key_Q:
+            print("Key \"Q\" pressed... Exiting Application...")
+            QApplication.instance().quit()
 
 
 
