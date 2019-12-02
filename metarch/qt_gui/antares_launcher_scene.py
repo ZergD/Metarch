1e0a407e17c20d543f17c445e23b7e69e4d0208a
MM <marc.mozgawa@gmail.com>
2019-12-02 23:35:07 +0100

    Added SelectFolderButton to the Scene.

--- metarch/qt_gui/antares_launcher_scene.py
+++ metarch/qt_gui/antares_launcher_scene.py
@@ -3,18 +3,25 @@ from PySide2.QtWidgets import QGraphicsScene
 
 from metarch.qt_gui import scene_objects
 from metarch.qt_gui.scene_objects.circles import Circle
-from metarch.qt_gui.scene_objects.rect_buttons import RectButton
+from metarch.qt_gui.scene_objects.rect_buttons import RectButton, SelectFolderButton
 
 
 class AntaresLauncherScene(QGraphicsScene):
+    """
+    Size of the scene has to be: [1440, 810], ie,  x = -720, y, -405, width = 1440, height = 810
+    """
 
     def __init__(self):
         super(AntaresLauncherScene, self).__init__()
 
+        # ################## DATA PART ##################
+        self.simulations = []
+
         # ################## INIT SCENE ##################
         self.setBackgroundBrush(Qt.black)
         # self.setSceneRect(-1000, -1000, 2000, 2000)
-        self.setSceneRect(-500, -500, 1000, 1000)
+        # self.setSceneRect(-500, -500, 1000, 1000)
+        self.setSceneRect(-720, -405, 1440, 810)
 
         # self.addItem(Circle(QPoint(10, 10)))
 
@@ -35,16 +42,18 @@ class AntaresLauncherScene(QGraphicsScene):
         #     sim = RectButton(position[0], position[1] - 400, 50, 200, "Hello")
         #     self.addItem(sim)
 
-        for i in range(1, 10):
-            x = -400
-            y = i * 100 - 500
-            width = 200
-            height = 50
-            sim = RectButton(x, y, height, width, "test")
-            self.addItem(sim)
+        # select_folder = RectButton(0, -525, 300, 75, "Select Folder")
+        select_folder = SelectFolderButton(-160, -525, 300, 75, "Select Folder")
+        self.addItem(select_folder)
 
+        # for i in range(1, 10):
+        #     x = -400
+        #     y = i * 100 - 500
+        #     width = 200
+        #     height = 50
+        #     sim = RectButton(x, y, width, height, "test")
+        #     self.addItem(sim)
 
-
-
-
-
+    # def mousePressEvent(self, event):
+    #     if event.buttons() == Qt.LeftButton:
+    #         print(f"You clicked on position [{event.scenePos().x()}, {event.scenePos().y()}]")
--- metarch/qt_gui/scene_objects/rect_buttons.py
+++ metarch/qt_gui/scene_objects/rect_buttons.py
@@ -1,12 +1,18 @@
 import numpy as np
 from PySide2.QtCore import Qt, QRectF
-from PySide2.QtWidgets import QGraphicsRectItem, QGraphicsItem
+from PySide2.QtGui import QFont
+from PySide2.QtWidgets import QGraphicsRectItem, QGraphicsItem, QFileDialog
 
 from metarch.qt_gui import scene_objects
+from pathlib import Path
+
+from metarch.qt_gui.scene_objects.texts import HText
 
 
 class RectButton(QGraphicsRectItem):
-    def __init__(self, x, y, height, width, text):
+    ID = 0
+
+    def __init__(self, x, y, width, height, text):
         """
         A Rectangle with some text in it
         :param text: str
@@ -15,6 +21,9 @@ class RectButton(QGraphicsRectItem):
         :param height: float
         :param width: float
         """
+        self.id = RectButton.ID
+        RectButton.ID += 1
+        print("my self.id = ", str(self.id))
         self.x = x
         self.y = y
         self.width = width
@@ -44,10 +53,20 @@ class RectButton(QGraphicsRectItem):
         self.current_color_2 = Qt.gray
         self.i = 0
         print("################################################################################\n")
+        self.qfile_name = QFileDialog()
 
     def mousePressEvent(self, q_mouse_event):
         # parent = self.parentItem()
         # parent.mousePressEvent(q_mouse_event)
+
+        if self.id == 0:
+            # print("we create a QFileDialog")
+            # qfile_name = QFileDialog().getOpenFileName("Choose a Simulation Folder", str(Path.cwd()))
+            qfile_name = self.qfile_name.getExistingDirectory()
+            # print("get existing directory : ", QFileDialog().getExistingDirectory())
+            # qfile_name = QFileDialog().getOpenFileName()
+            print("You choose the file directory: ", qfile_name)
+            self.id += 1
 
         self.i = (self.i + 1) % 2
         print("self.i = ", self.i)
@@ -67,16 +86,17 @@ class RectButton(QGraphicsRectItem):
         #         self.parent.mousePressEvent(q_mouse_event)
 
     def boundingRect(self):
-        offset = 5
-        return QRectF(self.x - offset, self.y - offset, self.width * 3.25, self.height + 10)
+        offset = 0.5
+        # return QRectF(self.x - offset, self.y - offset, self.width * 3.25, self.height + 10)
+        return QRectF(self.x - offset, self.y - offset, self.width * 3 + 4, self.height)
 
     def paint(self, qpainter, qstyle_option_graphics_item, widget=None):
         qpainter.setPen(Qt.NoPen)
         qpainter.drawRect(self.x, self.y, self.width, self.height)
 
         current_index_state = np.where(self.state == 1)[0][0]
-        print("PRINT")
-        print("current index is, ", current_index_state)
+        # print(f"RectButton[{self.id}]Being Painted...")
+        # print("current index is, ", current_index_state)
 
         if current_index_state == 0:
             self.current_color_1 = Qt.red
@@ -144,3 +164,121 @@ class SimulationTab(QGraphicsItem):
     def paint(self, qpainter, qstyle_option_graphics_item, widget=None):
         qpainter.setPen(self.mpen)
         qpainter.drawRect(self.x, self.y, self.width, self.height)
+
+
+class SelectFolderButton(QGraphicsRectItem):
+    ID = 0
+
+    def __init__(self, x, y, width, height, text):
+        """
+        A Rectangle with some text in it
+        :param text: str
+        :param x: float
+        :param y: float
+        :param height: float
+        :param width: float
+        """
+        self.id = SelectFolderButton.ID
+        SelectFolderButton.ID += 1
+        print("my self.id = ", str(self.id))
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+        super(SelectFolderButton, self).__init__(self.x, self.y, self.width, self.height)
+
+        # self.text = HText("Select Folder", self.x + self.width - 20, self.y + height / 2, self, 45)
+        # self.text = HText("Select Folder", self.x + 5, self.y + 5, self, 45)
+
+        # simple state array, represented as a numpy array, 0 = False, 1 = True
+        # [0] = ZIPPED, [1] = SENT, [2] = SUBMITTED, [3] = FINISHED, [4] = DELIVERED
+        self.state = np.zeros(5)
+        self.state[0] = 1
+        print(self.state.any())
+        # indexes = np.array(np.where(self.state == 1))
+        # indexes = list(np.where(self.state == 1))
+        indexes = np.where(self.state == 1)
+        print("TYPE")
+        print(type(indexes))
+        # indexes.append(3)
+        print("index state = ", indexes)
+        print("len = ", len(indexes))
+
+        print("state = \n", self.state)
+
+        self.mpen = scene_objects.initialize_qpen(Qt.gray, width=2)
+        self.current_color = Qt.darkRed
+        self.current_color_1 = Qt.gray
+        self.current_color_2 = Qt.gray
+        self.i = 0
+        print("################################################################################\n")
+        self.qfile_name = QFileDialog()
+
+    def mousePressEvent(self, q_mouse_event):
+        # parent = self.parentItem()
+        # parent.mousePressEvent(q_mouse_event)
+
+        if self.id == 0:
+            # print("we create a QFileDialog")
+            # qfile_name = QFileDialog().getOpenFileName("Choose a Simulation Folder", str(Path.cwd()))
+            qfile_name = self.qfile_name.getExistingDirectory()
+            # print("get existing directory : ", QFileDialog().getExistingDirectory())
+            # qfile_name = QFileDialog().getOpenFileName()
+            print("You choose the file directory: ", qfile_name)
+            self.id += 1
+
+        self.i = (self.i + 1) % 2
+        print("self.i = ", self.i)
+
+        self.state = np.zeros(5)
+        self.state[self.i] = 1
+
+        self.update()
+
+        # print("you pressed on RectButton with name = " + str(self.text))
+        # # print("this item has " + str(len(self.parents)) + " parents")
+        # if self.parent:
+        #     print(self.parent)
+        #     if isinstance(self.parent, Signal):
+        #         self.parent.mousePressEvent(q_mouse_event)
+        #     else:
+        #         self.parent.mousePressEvent(q_mouse_event)
+
+    def boundingRect(self):
+        offset = 0.5
+        # return QRectF(self.x - offset, self.y - offset, self.width * 3.25, self.height + 10)
+        return QRectF(self.x - offset, self.y - offset, self.width * 3 + 4, self.height)
+
+    def paint(self, qpainter, qstyle_option_graphics_item, widget=None):
+        qpainter.setPen(Qt.NoPen)
+        qpainter.drawRect(self.x, self.y, self.width, self.height)
+
+        current_index_state = np.where(self.state == 1)[0][0]
+        # print(f"RectButton[{self.id}]Being Painted...")
+        # print("current index is, ", current_index_state)
+
+        if current_index_state == 0:
+            self.current_color_1 = Qt.red
+            # self.current_color_1 = Qt.gray
+        else:
+            self.current_color_1 = Qt.gray
+        if current_index_state == 1:
+            self.current_color_2 = Qt.red
+        else:
+            self.current_color_2 = Qt.gray
+
+        # first rectangle, name of simulation
+        qpainter.setBrush(self.current_color)
+        qpainter.drawRect(self.x, self.y, self.width, self.height)
+
+        qpainter.setPen(self.mpen)
+        # surrounding design
+        qrect_surround = QRectF(self.x - 2, self.y - 2, self.width + 4, self.height + 4)
+        qpainter.drawRect(qrect_surround)
+
+        # Print Label
+        qrect = QRectF(self.x, self.y, self.width, self.height)
+        qpainter.setPen(Qt.white)
+        qpainter.setFont(QFont("Times", 40))
+        qpainter.drawText(qrect, Qt.AlignCenter, self.text)
